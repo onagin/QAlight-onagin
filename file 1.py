05041882bacdb2540d36736a984bc55a4b981081
onagin <ol4sound@gmail.com>
2020-05-25 17:30:34 +0300

    my 1 home work

---
+++ file 1.py
@@ -0,0 +1,15 @@
+a1 = 'New'
+a2 = ' York'
+print(a1 + a2)
+a = 5
+b = 2
+print(a * b)
+print(a / b)
+print(a + b)
+print(a - b)
+c = 20.0
+d = 5.0
+print(c * d)
+print(c / d)
+print(c + d)
+print(c - d)
